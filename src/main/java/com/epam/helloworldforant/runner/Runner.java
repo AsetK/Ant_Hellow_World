3488693acccc7986579e6fc828e5493b381c7888
Asset_Kenezhanov <bysskc@mail.ru>
2019-05-31 17:23:43 +0600

    2-commit

--- src/main/java/com/epam/helloworldforant/runner/Runner.java
+++ src/main/java/com/epam/helloworldforant/runner/Runner.java
@@ -1,6 +1,7 @@
 package com.epam.helloworldforant.runner;
 
 import com.epam.helloworldforant.service.PrinterInterface;
+import org.apache.commons.lang3.StringUtils;
 
 import java.util.Iterator;
 import java.util.ResourceBundle;
@@ -9,20 +10,33 @@ import java.util.ServiceLoader;
 public class Runner {
 
     public static void main(String[] args) {
-        String name = args[0];
+//        String name = args[0];
+//
+//        ServiceLoader<PrinterInterface> loader = ServiceLoader.load(PrinterInterface.class);
+//        Iterator<PrinterInterface> iterator = loader.iterator();
+//
+//        PrinterInterface printer = null;
+//
+//        if(iterator.hasNext()){
+//            printer = iterator.next();
+//        }
+////        ResourceBundle resourceBundle = ResourceBundle.getBundle("properties.message");
+//        ResourceBundle resourceBundle = ResourceBundle.getBundle("properties.message_"+ name);
+//        String message = resourceBundle.getString("greeting");
+//
+//        printer.print(message + name);
 
-        ServiceLoader<PrinterInterface> loader = ServiceLoader.load(PrinterInterface.class);
-        Iterator<PrinterInterface> iterator = loader.iterator();
+//        int l = args.length;
+//        for (int i = 0; i<l; i++ )
+//        {
+//            System.out.println(args[i]);
+//        }
+//
+//        System.out.println(System.getProperty("arg3"));
+//        System.out.println(System.getenv("arg4"));
 
-        PrinterInterface printer = null;
+        System.out.println(StringUtils.isEmpty(""));
 
-        if(iterator.hasNext()){
-            printer = iterator.next();
-        }
-        ResourceBundle resourceBundle = ResourceBundle.getBundle("properties.message");
-        String message = resourceBundle.getString("greeting");
-
-        printer.print(message + name);
 
     }
 }
